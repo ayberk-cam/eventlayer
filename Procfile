4681119688f8cc796092139414dc6619212e715e
Ayberk Çam <cama16@itu.edu.tr>
2021-01-19 09:42:53 +0300

    make it better

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn server:app
+web: gunicorn app:app
 
 web: gunicorn run:app
--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from database import Database
 POSTGRESQL_URI = "postgres://qesotqlegqoojv:9eb7d18c5bdd26cc185286da84ad2857b52474c1c07c03526b03af1cf7d6d740@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d1pkt0mjn6nlqi"
 db = Database(POSTGRESQL_URI)
 
+app = Flask(__name__)
+
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object("settings")
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/register", view_func=views.register_page)
     app.add_url_rule("/studentregister", view_func=views.studentregister_page, methods=["GET", "POST"])
